265bdf666d375808ae1dd98c04b20cb7ff2a7ac5
alankin <460682402@qq.com>
2018-04-10 17:16:07 +0800

    详情页

---
+++ js/detail.js
@@ -0,0 +1,23 @@
+$(window).ready(function () {
+    // generateCatalog(".left nav");
+});
+
+function generateCatalog(selector) {
+    var _containerSelector = 'div.post-container';
+    var P = $(_containerSelector);
+    var $h2 = P.find('h2');
+    // clean
+    $(selector).empty();
+    var $dl = $("<dl></dl>");
+    $(selector).append($dl);
+    $h2.each(function () {
+        var h2self = $(this);
+        var cath2 = $("<dt><em></em>" + h2self.attr("id") + "</dt>");
+        $dl.append(cath2);
+        var $h3 = h2self.nextUntil("h2").filter("h3");
+        $h3.each(function () {
+            cath2.after("<dd class=\"\"><a href=\""+$(this).children("a").attr("href")+"\" class=\"\">" + $(this).attr("id") + "<span></span></a></dd>");
+        });
+    });
+    return true;
+}
